2d2d8d658caf29db23108bb24c7b8c3f430fa824
Eduard-Soft <Eduard_gallardo@soy.sena.edu.co>
2025-08-26 17:56:56 -0500

    fourth commit

--- routes/login.py
+++ routes/login.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 from utils.conexion_db import get_db_connection
 
 login_bp = Blueprint('login', __name__)
-@login_bp.route('/')
+@login_bp.route('/login')
 def login():
     return render_template('login.html')

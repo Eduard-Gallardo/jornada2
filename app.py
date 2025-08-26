da99f3a0552000097e3031436340fd07059732b5
Eduard-Soft <Eduard_gallardo@soy.sena.edu.co>
2025-08-26 17:54:18 -0500

    fourth commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from routes.login import login_bp
 
 app = Flask(__name__)
+
+app.register_blueprint(login_bp)
 
 @app.route('/')
 def index():
--- routes/login.py
+++ routes/login.py
@@ -1,7 +1,7 @@
-from flask import blueprint, render_template
+from flask import Blueprint, render_template
 from utils.conexion_db import get_db_connection
 
-login_bp = blueprint('login', __name__)
-@login_bp.route('/login')
+login_bp = Blueprint('login', __name__)
+@login_bp.route('/')
 def login():
     return render_template('login.html')
